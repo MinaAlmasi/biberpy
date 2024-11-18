f1770ba539acd48f1403a188d5a0aed169d1d630
MinaAlmasi <mina.almasi00@gmail.com>
2024-11-18 11:29:45 +0100

    feat: update to fit my workflow (only load ndjson)

--- biber-dim.py
+++ biber-dim.py
@@ -557,11 +557,10 @@ def readnumlist(f):
 
 parser = argparse.ArgumentParser(description="Multilingual Biber-like tagger")
 parser.add_argument('-1', '--embeddings', type=str, help='embeddings, not implemented yet')
-parser.add_argument('-f', '--format', type=str, default='ol', help='either ol (default) or json output by conll2json.py')
 parser.add_argument('-l', '--language', type=str, default='en', help='language id for getting the annotation files')
 parser.add_argument('-s', '--suppressheader', action='store_true', help='suppresses the default feature header')
 parser.add_argument('-v', '--verbosity', type=int, default=1)
-parser.add_argument('-i', '--infile', nargs='?', type=argparse.FileType('r'), default=sys.stdin, help='one-doc-per-line corpus, plain or json')
+parser.add_argument('-i', '--infile', nargs='?', default=sys.stdin, help='one-doc-per-line corpus, plain or json')
 parser.add_argument('-o', '--outfile', nargs='?', type=argparse.FileType('w'), default=sys.stdout, help='output file')
 parser.add_argument('-t', '--test', type=str, default='', help='Test only this function')
 
@@ -569,7 +568,6 @@ args = parser.parse_args()
 ut.verbosity=args.verbosity
 language=args.language
 dirname=os.path.dirname(os.path.realpath(__file__))
-assert args.format in ['ol','json'], 'Wrong format, either ol or json is accepted. Requested: '+args.format
 
 wordlists = readwordlists(open(dirname+'/'+language+'.properties', encoding="utf8"))
 if args.format=='ol':
@@ -585,13 +583,14 @@ if not args.suppressheader:
 if args.verbosity>1:
     print('total %d dims in output' % len(dimnames),file=sys.stderr)
 
+
+with open(args.infile, 'r') as f:
+    data = [json.loads(line) for line in f]
+
+text = [row["human_completions"] for row in data]
+
 starttime=time.time()
-for i,line in enumerate(args.infile):
-    if args.format=='ol':
-        docstring=line.strip().lower()
-        doc=docstring.split()
-    elif args.format=='json':
-        doc=json.loads(line)
+for i, doc in enumerate(text):
     dims=getbiberdims(doc)
     if args.test:
         print(dims,file=args.outfile)
